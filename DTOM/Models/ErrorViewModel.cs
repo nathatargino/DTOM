582135ba53c95cf57523b035db6ff9fa114d6d73
Nathã Targino <148729150+nathatargino@users.noreply.github.com>
2026-01-29 14:50:21 -0300

    Adicionar arquivos de projeto.

---
+++ DTOM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DTOM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
